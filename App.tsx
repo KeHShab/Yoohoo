9a1173ea4a9ff3fee6d7f03a6f228c0669d0ad5a
Keshabyss <184175376+KeHShab@users.noreply.github.com>
2024-10-10 10:23:05 +0900

    boolean

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './src/components/hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* {<Hello/>} */}
+      {/* {<Hello>World</Hello>} */}
+      {/* {<Hello/>World</Hello>} */}
+    
+      <Hello bang = {false}> World</Hello>
       <Text>Hey, Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ src/components/hello.tsx
@@ -0,0 +1,29 @@
+
+import { StyleSheet, Text, View } from 'react-native'
+
+interface Props{
+    children: string
+    bang: boolean
+}
+
+const hello = (props:Props): JSX.Element => {
+    const {children, bang} = props
+    return (
+        <View>
+            <Text style= { style.text}>Hello{children}{bang ? '!': ""}</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    text:{
+        color:"#FFFFFF",
+        backgroundColor: "blue",
+        fontSize: 40,
+        fontWeight: "bold",
+        padding: 16
+    }
+})
+
+
+export default hello
